73cce358f5ac722bce63afc261e0fba97c34b1c3
pony <ponychen.cs13@nycu.edu.tw>
2025-06-16 03:20:31 +0800

    add cpu_monitor

--- sFlow_2/cpu_monitor.sh
+++ sFlow_2/cpu_monitor.sh
@@ -3,14 +3,14 @@
 output_file="cpu_record.txt"
 > "$output_file"  # 清空舊檔
 
-echo "Collecting CPU usage every 0.5s, total 20 times..."
+echo "Collecting CPU usage every 0.1s, total 100 times..."
 
-for i in {1..20}; do
-    timestamp=$(date +"%H:%M:%S")
+for i in {1..100}; do
+    timestamp=$(date +"%H:%M:%S.%3N")
     usage=$(top -bn1 | awk 'NR > 7 && $9 ~ /^[0-9.]+$/ { sum += $9 } END { print sum }')
     echo "$timestamp $usage" >> "$output_file"
     echo "[$i] $timestamp → CPU: $usage%"
-    sleep 0.5
+    sleep 0.1
 done
 
 # 計算平均
