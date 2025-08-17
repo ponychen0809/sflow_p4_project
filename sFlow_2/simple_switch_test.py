390f6718841ed85d9f5c0055644ea67425568436
pony <ponychen.cs13@nycu.edu.tw>
2025-08-17 23:31:39 +0800

    multi thread

--- sFlow_2/simple_switch_test.py
+++ sFlow_2/simple_switch_test.py
@@ -280,7 +280,7 @@ class SimpleSwitchTest(BfRuntimeTest):
                         # print(11111)
                         
                         raw = bytes(pkt)
-                        print(raw.__len__)
+                        print("raw len: ",len(raw))
                         global write_count
                         write_count = write_count +1
                         print("write_count: " ,write_count)
