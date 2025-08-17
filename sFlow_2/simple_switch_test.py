e9237cd0c256f489a7062cb2389f325ac1d0082b
pony <ponychen.cs13@nycu.edu.tw>
2025-08-17 20:54:02 +0800

    multi thread

--- sFlow_2/simple_switch_test.py
+++ sFlow_2/simple_switch_test.py
@@ -307,7 +307,7 @@ class SimpleSwitchTest(BfRuntimeTest):
                 pkt_count = pkt_count+1
                 # print("receive packet: ",pkt_count)
                 mirror = Mirror(raw[MIRRORING_METADATA_OFFSET:MIRRORING_METADATA_OFFSET+MIRRORING_METADATA_LENGTH])
-                print("total packet: ",mirror.total_packets)
+                # print("total packet: ",mirror.total_packets)
                 ethernet = Ether(raw[ETHERNET_HEADER_OFFSET:ETHERNET_HEADER_OFFSET+ETHERNET_HEADER_LENGTH])
                 if (ethernet.type != TYPE_IPV4):
                     continue
