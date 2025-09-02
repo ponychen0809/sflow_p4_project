76a3df33200a2ab02fec01e83fa3621f8c279c46
pony <ponychen.cs13@nycu.edu.tw>
2025-09-02 17:56:31 +0800

    add getpid

--- sFlow_2/simple_switch_test.py
+++ sFlow_2/simple_switch_test.py
@@ -293,7 +293,8 @@ class SimpleSwitchTest(BfRuntimeTest):
                     packet = queue.get()
                     handle_pkt_count.value += 1
                     f.write("handle_pkt_count: ",handle_pkt_count.value,"\n")
-                    # print("handle_pkt_count: ", handle_pkt_count.value)
+                    f.close()
+                    print("handle_pkt_count: ", handle_pkt_count.value)
                     handle_pkt(packet, agent, None, pkt_count,error_count,write_count,queue_max,queue)  # 假設沒有實際的 mirror 參數
                     
                 # else:
